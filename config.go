71027311ce57619a55dc546f0363164a706394d7
cprosko <cprosko@gmail.com>
2025-12-26 13:54:42 +0100

    Added parseConfig func for parsing yaml

--- config.go
+++ config.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	// Standard Packages
+	"log"
+	"os"
 
 	// External Packages
 	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
+	Token          string   `yaml:"bot_token"`
+	ServerID       string   `yaml:"server_id"`
 	VoiceChannelID string   `yaml:"voice_channel_id"`
 	MinInterval    int      `yaml:"min_interval_seconds"`
 	MaxInterval    int      `yaml:"max_interval_seconds"`
@@ -15,6 +19,14 @@ type Config struct {
 }
 
 func parseConfig(path string) Config {
-	// TODO: not implemented
-	return Config{}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Unable to read file at path %s:\n%s\n", path, err)
+	}
+	var conf Config
+	err = yaml.Unmarshal(data, &conf)
+	if err != nil {
+		log.Fatalf("Unable to parse YAML in file %s: \n%s\n", path, err)
+	}
+	return conf
 }
